2618366a053b440a9efd5ce2e0bfc13db5da20a5
developervicky <developer.vicky6@gmail.com>
2025-03-05 01:27:23 +0530

    refactor: removed the params

--- app/(crew)/crew/[id]/page.tsx
+++ app/(crew)/crew/[id]/page.tsx
@@ -1,17 +1,6 @@
-import { connectDB } from "@/lib/mongoose";
-import { Crew } from "@/models/Crew";
 import React from "react";
 
-interface PageProps {
-  params: { id: string };
-}
-const CrewIdPage = async ({ params }: PageProps) => {
-  const { id } = params;
-  await connectDB();
-  const crew = await Crew.findOne({ _id: id }).populate("members");
-
-  console.log(crew.members);
-
+const CrewIdPage = async () => {
   return <div>CrewIdPage</div>;
 };
 
